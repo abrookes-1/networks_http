38c48bd43ccee80f0d7c1e8f17658be0943b6d32
Aidan Brookes <abrookes@mines.edu>
2020-12-17 07:14:09 -0700

    first commit

---
+++ web_server.cc
@@ -0,0 +1,168 @@
+// **************************************************************************************
+// * Echo Strings (echo_s.cc)
+// * -- Accepts TCP connections and then echos back each string sent.
+// **************************************************************************************
+#include "web_server.h"
+#include <iostream>
+
+// **************************************************************************************
+// * processConnection()
+// * - Handles reading the line from the network and sending it back to the client.
+// * - Returns 1 if the client sends "QUIT" command, 0 if the client sends "CLOSE".
+// **************************************************************************************
+int processConnection(int sockFd) {
+  char buffer[1024] = {0};
+  char *token;
+  int quitProgram = 0;
+  int keepGoing = 1;
+  int result;
+  std::string msg;
+  buffer[0] = '\0';
+
+  DEBUG << "Waiting for data" << std::endl;
+  while (keepGoing) {
+    result = read(sockFd, buffer, 1024);
+    if (buffer[0] != '\0'){
+      std::cout << "|" << buffer << "|" << std::endl;
+    }
+
+    if (buffer[0] != '\0'){
+      DEBUG << "Received message: " << buffer;
+      if (strlen(buffer) > 4 && strncmp(buffer, "CLOSE", 5) == 0){
+        std::cout << "Connection closed by client." << std::endl;
+        close(sockFd);
+        keepGoing = 0;
+      }
+      else if (strlen(buffer) > 3 && strncmp(buffer, "QUIT", 4) == 0){
+        std::cout << "Server shutdown by client." << std::endl;
+        close(sockFd);
+        keepGoing=0;
+        quitProgram=1;
+      }
+      else {
+        write(sockFd, buffer, strlen(buffer));
+      }
+    }
+
+    //
+    // Call write() to send line back to the client.
+    //
+
+    buffer[0] = '\0';
+  }
+  keepGoing = 1;
+  return quitProgram;
+}
+    
+
+
+// **************************************************************************************
+// * main()
+// * - Sets up the sockets and accepts new connection until processConnection() returns 1
+// **************************************************************************************
+
+int main (int argc, char *argv[]) {
+
+  // ********************************************************************
+  // * Process the command line arguments
+  // ********************************************************************
+  boost::log::add_console_log(std::cout, boost::log::keywords::format = "%Message%");
+  boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::info);
+
+  // ********************************************************************
+  // * Process the command line arguments
+  // ********************************************************************
+  int opt = 0;
+  while ((opt = getopt(argc,argv,"v")) != -1) {
+    switch (opt) {
+    case 'v':
+      std::cout << "Verbose mode selected\n";
+      boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::debug);
+      break;
+    case ':':
+    case '?':
+    default:
+      std::cout << "useage: " << argv[0] << " -v" << std::endl;
+      exit(-1);
+    }
+  }
+
+  // *******************************************************************
+  // * Creating the inital socket is the same as in a client.
+  // ********************************************************************
+  int     listenFd = -1;
+       // Call socket() to create the socket you will use for lisening.
+  DEBUG << "Calling Socket() assigned file descriptor " << listenFd << ENDL;
+  listenFd = socket(PF_INET, SOCK_STREAM, 0);
+  DEBUG << "Socket opened" << std::endl;
+  // ********************************************************************
+  // * The bind() and calls take a structure that specifies the
+  // * address to be used for the connection. On the cient it contains
+  // * the address of the server to connect to. On the server it specifies
+  // * which IP address and port to lisen for connections.
+  // ********************************************************************
+  struct sockaddr_in servaddr;
+  srand(time(NULL));
+  int port = (rand() % 10000) + 1024;
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = PF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(port);
+  int addrlen = sizeof(servaddr);
+
+
+  // ********************************************************************
+  // * Binding configures the socket with the parameters we have
+  // * specified in the servaddr structure.  This step is implicit in
+  // * the connect() call, but must be explicitly listed for servers.
+  // ********************************************************************
+  DEBUG << "Calling bind(" << listenFd << "," << &servaddr << "," << sizeof(servaddr) << ")" << ENDL;
+  int bindSuccesful = -1;
+  while (bindSuccesful<0) {
+    port = (rand() % 10000) + 1024;
+    servaddr.sin_port = htons(port);
+    // You may have to call bind multiple times if another process is already using the port
+    // your program selects.
+    bindSuccesful = bind(listenFd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+  }
+  DEBUG << "Socket bound" << std::endl;
+  std::cout << "Using port " << port << std::endl;
+
+
+  // ********************************************************************
+  // * Setting the socket to the listening state is the second step
+  // * needed to being accepting connections.  This creates a queue for
+  // * connections and starts the kernel listening for connections.
+  // ********************************************************************
+  int listenQueueLength = 1;
+  DEBUG << "Calling listen(" << listenFd << "," << listenQueueLength << ")" << ENDL;
+  listen(listenFd, listenQueueLength);
+
+  // ********************************************************************
+  // * The accept call will sleep, waiting for a connection.  When 
+  // * a connection request comes in the accept() call creates a NEW
+  // * socket with a new fd that will be used for the communication.
+  // ********************************************************************
+  int quitProgram = 0;
+  while (!quitProgram) {
+    int connFd = 0;
+
+    DEBUG << "Calling accept(" << listenFd << "NULL,NULL)." << ENDL;
+    DEBUG << "Waiting for connection..." << std::endl;
+    connFd = accept(listenFd, (struct sockaddr *)&servaddr, (socklen_t*)&addrlen);
+    // The accept() call checks the listening queue for connection requests.
+    // If a client has already tried to connect accept() will complete the
+    // connection and return a file descriptor that you can read from and
+    // write to. If there is no connection waiting accept() will block and
+    // not return until there is a connection.
+    DEBUG << "We have recieved a connection on " << connFd << ENDL;
+
+    
+    quitProgram = processConnection(connFd);
+   
+    close(connFd);
+  }
+
+  close(listenFd);
+
+}
